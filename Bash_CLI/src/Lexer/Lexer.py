6a68aa8577913235897d0d8762319001aa00aa4e
Saveliy Grigoryev <sava-cska@mail.ru>
2022-10-01 23:18:46 +0300

    Add pipe and envvars

--- Bash_CLI/src/Lexer/Lexer.py
+++ Bash_CLI/src/Lexer/Lexer.py
@@ -1,5 +1,6 @@
 from typing import List
 from itertools import chain
+import string
 
 from src.Controller import Controller
 
@@ -9,6 +10,7 @@ class Lexer(object):
 
     def __init__(self, controller: Controller):
         self.controller = controller
+        self._variable_symbols = set(string.ascii_letters) | set(string.digits) | set('_')
     
     def _process_equation_and_space(self, arg_str: str) -> List[str]:
         """Split the string for the symbols " " and "="."""
@@ -25,6 +27,26 @@ class Lexer(object):
         
         tokens = chain.from_iterable(tokens)
         return list(filter(lambda s: len(s) > 0, tokens))
+
+    def _process_environment_variables(self, arg_str: str) -> List[str]:
+        """Find usage of environment variables and create tokens from it."""
+        idx = 0
+        tokens = []
+        last_split = 0
+        while idx < len(arg_str):
+            if arg_str[idx] == '$':
+                tokens.append(arg_str[last_split:idx])
+                idx_var = idx + 1
+                while idx_var < len(arg_str) and arg_str[idx_var] in self._variable_symbols:
+                    idx_var += 1
+                print(arg_str[idx:idx_var])
+                tokens.append(arg_str[idx:idx_var])
+                idx = idx_var
+                last_split = idx_var
+            else:
+                idx += 1
+        tokens.append(arg_str[last_split:])
+        return list(filter(lambda s: len(s) > 0, tokens))
     
     def _process_double_quotation(self, arg_str: str) -> List[str]:
         """Split the string for the symbol "\"" and process each substring separately."""
@@ -33,12 +55,17 @@ class Lexer(object):
         final_tokens = []
         for idx, s in enumerate(tokens):
             if idx % 2 == 0:
-                final_tokens.extend(self._process_equation_and_space(s))
+                after_subst = self._process_environment_variables(s)
+                for t in after_subst:
+                    if not t.startswith('$'):
+                        final_tokens.extend(self._process_equation_and_space(t))
+                    else:
+                        final_tokens.append(t)
             else:
-                new_s = '"' + s
+                final_tokens.append('"')
+                final_tokens.extend(self._process_environment_variables(s))
                 if idx != len(tokens) - 1:
-                    new_s += '"'
-                final_tokens.append(new_s)
+                    final_tokens.append('"')
         return list(filter(lambda s: len(s) > 0, final_tokens))
 
     def _process_single_quotation(self, arg_str: str) -> List[str]:
@@ -46,40 +73,56 @@ class Lexer(object):
 
         open_single_quotation = False
         open_double_quotation = False
-        current_str = ''
         tokens = []
-        for symb in arg_str:
+        last_split = 0
+        for idx, symb in enumerate(arg_str):
             if symb == "'":
                 if open_single_quotation:
-                    current_str += "'"
-                    tokens.append(current_str)
+                    tokens.append(arg_str[last_split:(idx + 1)])
                     open_single_quotation = False
-                    current_str = ''
-                elif open_double_quotation:
-                    current_str += symb
-                else:
-                    tokens.extend(self._process_double_quotation(current_str))
+                    last_split = idx + 1
+                elif not open_double_quotation:
+                    tokens.extend(self._process_double_quotation(arg_str[last_split:idx]))
                     open_single_quotation = True
-                    current_str = "'"
+                    last_split = idx
             elif symb == '"':
                 if open_double_quotation:
                     open_double_quotation = False
                 elif not open_single_quotation:
                     open_double_quotation = True
-                current_str += symb
-            else:
-                current_str += symb
         
         if open_single_quotation:
-            tokens.append(current_str)
+            tokens.append(arg_str[last_split:])
         else:
-            tokens.extend(self._process_double_quotation(current_str))
+            tokens.extend(self._process_double_quotation(arg_str[last_split:]))
         return list(filter(lambda s: len(s) > 0, tokens))
 
     def _process_pipe(self, arg_str: str) -> List[str]:
         """Split the string for the symbol '|' and process each substring separately."""
 
-        return self._process_single_quotation(arg_str)
+        open_single_quotation = False
+        open_double_quotation = False
+        tokens = []
+        last_split = 0
+        for idx, symb in enumerate(arg_str):
+            if symb == '|':
+                if not open_single_quotation and not open_double_quotation:
+                    tokens.extend(self._process_single_quotation(arg_str[last_split:idx]))
+                    tokens.append('|')
+                    last_split = idx + 1
+            if symb == '"':
+                if open_double_quotation:
+                    open_double_quotation = False
+                elif not open_single_quotation:
+                    open_double_quotation = True
+            if symb == "'":
+                if open_single_quotation:
+                    open_single_quotation = False
+                elif not open_double_quotation:
+                    open_single_quotation = True
+
+        tokens.extend(self._process_single_quotation(arg_str[last_split:]))
+        return list(filter(lambda s: len(s) > 0, tokens))
 
     def parse_to_tokens(self, arg_str: str) -> List[str]:
         """Generate array of tokens from raw command line."""
