8f6b2656e0b995f137674b835866c28737683148
Saveliy Grigoryev <sava-cska@mail.ru>
2022-10-01 23:18:46 +0300

    Add pipe and envvars

--- Bash_CLI/test/Lexer/test_Lexer.py
+++ Bash_CLI/test/Lexer/test_Lexer.py
@@ -21,7 +21,7 @@ def test_parse_to_tokens_3():
     lexer = Lexer(controller)
 
     assert lexer.parse_to_tokens('pwd "home/user/home = ab" "s=t" -v ') == \
-           ['pwd', '"home/user/home = ab"', '"s=t"', '-v']
+           ['pwd', '"', 'home/user/home = ab', '"', '"', 's=t', '"', '-v']
 
 
 def test_parse_to_tokens_4():
@@ -36,4 +36,30 @@ def test_parse_to_tokens_5():
     lexer = Lexer(controller)
 
     assert lexer.parse_to_tokens('abc \'"raz, dva"\' " \' aaaa \' hhhhh \' " \' fffff') == \
-           ['abc', '\'"raz, dva"\'', "\" ' aaaa ' hhhhh ' \"", "' fffff"]
+           ['abc', '\'"raz, dva"\'', '"', " ' aaaa ' hhhhh ' ", '"', "' fffff"]
+
+def test_parse_to_tokens_6():
+    controller = Controller()
+    lexer = Lexer(Controller)
+
+    assert lexer.parse_to_tokens('$x$y = 15') == ['$x', '$y', '=', '15']
+
+def test_parse_to_tokens_7():
+    controller = Controller()
+    lexer = Lexer(Controller)
+
+    assert lexer.parse_to_tokens('x = ex | y = it|$x$y') == ['x', '=', 'ex', '|', 'y', '=', 'it', '|', '$x', '$y']
+
+def test_parse_to_tokens_8():
+    controller = Controller()
+    lexer = Lexer(Controller)
+
+    assert lexer.parse_to_tokens('$x\'aboba$y\' "$x$y" \'"$r\' = 15') == \
+        ['$x', '\'aboba$y\'', '"', '$x', '$y', '"', '\'"$r\'', '=', '15']
+
+def test_parse_to_tokens_9():
+    controller = Controller()
+    lexer = Lexer(Controller)
+
+    assert lexer.parse_to_tokens('wc | cat " | echo $___12" \' x=ex | $123 \' | pwd') == \
+        ['wc', '|', 'cat', '"', ' | echo ', '$___12', '"', '\' x=ex | $123 \'', '|', 'pwd']
