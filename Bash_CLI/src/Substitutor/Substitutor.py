5be769a50e52f93eeaecfbb9fd54cecf0c7e8b6c
Alex <113362960+AlexPolarBear@users.noreply.github.com>
2022-10-03 19:37:48 +0300

    Update Substitutor.py

--- Bash_CLI/src/Substitutor/Substitutor.py
+++ Bash_CLI/src/Substitutor/Substitutor.py
@@ -20,6 +20,8 @@ class Substitutor(object):
                 return ''.join(list_tokens)
             start += 1
             end += 1
+            if tokens.find('$'):
+                return ''.join(list_tokens)
         return ' '.join(list_tokens)
     
     # def resolve_env_var(self, list_tokens, mapper: Controller[str]) -> str:
