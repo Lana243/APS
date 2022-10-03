8e100cab573f39a2f350faf1a4468ed1be4852a9
Alex <113362960+AlexPolarBear@users.noreply.github.com>
2022-10-03 18:22:38 +0300

    Update Substitutor.py

--- Bash_CLI/src/Substitutor/Substitutor.py
+++ Bash_CLI/src/Substitutor/Substitutor.py
@@ -11,10 +11,10 @@ class Substitutor(object):
 
     def resolve_env_vars(self, list_tokens: List[str]) -> str:
         """Substitute environment variables in the list of tokens."""
-        for tokens in list_tokens:
-            while tokens != '"':
+        for list_tokens in List[str]:
+            while list_tokens != '"':
                 return ''.join(list_tokens)                    # пока только пробное
-            if tokens == '"':
+            if list_tokens == '"':
                 break
             else:
                 return ' '.join(list_tokens)
