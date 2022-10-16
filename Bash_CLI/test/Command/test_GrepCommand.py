dccb2fbabc63e9ed0deda70195054bd83b4e25e3
uvd174 <st075915@student.spbu.ru>
2022-10-16 21:59:41 +0300

    Added a couple of tests

--- Bash_CLI/test/Command/test_GrepCommand.py
+++ Bash_CLI/test/Command/test_GrepCommand.py
@@ -29,3 +29,31 @@ def test_grep_command_run_2():
     stdout, _, _ = command.run(input_text, controller)
 
     assert stdout == input_text
+
+
+def test_grep_command_run_3():
+    controller = Controller()
+
+    input_text = 'a b c\nd e f'
+
+    command = GrepCommand(args=[
+        Token('A', ' ', ''), Token('-A', ' ', ''), Token('1', '', ''), Token('-i', '', ''),
+    ])
+
+    stdout, _, _ = command.run(input_text, controller)
+
+    assert stdout == input_text
+
+
+def test_grep_command_run_4():
+    controller = Controller()
+
+    input_text = 'a b c\nab e f\ndA e f\ne AAa f'
+
+    command = GrepCommand(args=[
+        Token('A', ' ', ''), Token('-A', ' ', ''), Token('0', '', ''), Token('-i', '', ''), Token('-w', '', ''),
+    ])
+
+    stdout, _, _ = command.run(input_text, controller)
+
+    assert stdout == input_text.splitlines()[0]
