af099b921a374cc97fcb2b6d2dc087edf0fb3c54
uvd174 <st075915@student.spbu.ru>
2022-10-17 09:51:37 +0300

    Debugging...

--- Bash_CLI/src/Command/GrepCommand.py
+++ Bash_CLI/src/Command/GrepCommand.py
@@ -46,11 +46,14 @@ class GrepCommand(Command):
         str_args = list(map(str.strip, map(str, self.args)))
         print(type(str_args), str_args)
 
-        parser = argparse.ArgumentParser(prog='grep', description='Find matching patterns.', exit_on_error=False)
-        parser.add_argument('pattern', type=str, help='The pattern to search for.')
-        parser.add_argument('-w', action='store_true', help='Search for the whole word.')
-        parser.add_argument('-i', action='store_true', help='File to search in.')
-        parser.add_argument('-A', type=int, default=0, help='Print the number of lines after match.')
+        try:
+            parser = argparse.ArgumentParser(prog='grep', description='Find matching patterns.', exit_on_error=False)
+            parser.add_argument('pattern', type=str, help='The pattern to search for.')
+            parser.add_argument('-w', action='store_true', help='Search for the whole word.')
+            parser.add_argument('-i', action='store_true', help='File to search in.')
+            parser.add_argument('-A', type=int, default=0, help='Print the number of lines after match.')
+        except BaseException as exc:
+            print(exc)
 
         print(type(str_args), str_args)
         args = parser.parse_args(str_args)
